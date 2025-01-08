7ef80a9b1e8384f688454442dd5c16fc8bd8ac78
naveenprakashkv <naveen.prakash.kv@gmail.com>
2025-01-08 13:42:15 +0530

    this is push in the lisnked list

---
+++ array.js
@@ -0,0 +1,26 @@
+class Node {
+  constuctor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+}

96b22334690882518f6325a7ee2df2af683f0da5
naveenprakashkv <naveen.prakash.kv@gmail.com>
2025-01-17 17:31:15 +0530

    key method and porper comments added , set key update of the updating alrady exitstin key

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -2,6 +2,7 @@ class HashTable {
   constructor(size = 7) {
     this.dataMap = new Array(size);
   }
+  // hash function to find the hashcode and index for the corresponding hash key
   _hash(key) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
@@ -9,16 +10,47 @@ class HashTable {
     }
     return hash;
   }
-
+  // to set the key and corrsponding value in the hash table
   set(key, value) {
     let index = this._hash(key);
     if (!this.dataMap[index]) {
       this.dataMap[index] = [];
     }
+    // to check the key is already existed then update value
+    for (let i = 0; i < this.dataMap[index].length; i++) {
+      if (this.dataMap[index][i][0] === key) {
+        this.dataMap[index][i][1] = value;
+        return this;
+      }
+    }
     this.dataMap[index].push([key, value]);
     return this;
   }
+  //method to find the value corresponding to the key
+  get(key) {
+    let index = this._hash(key);
+    if (this.dataMap[index]) {
+      for (let i = 0; i < this.dataMap[index].length; i++) {
+        if (this.dataMap[index][i][0] === key) return this.dataMap[index][i][1];
+      }
+    }
+    return undefined;
+  }
+  // method to find all the key in the hash table
+  keys() {
+    let allKeys = [];
+    for (let i = 0; i < this.dataMap.length; i++) {
+      if (this.dataMap[i]) {
+        for (let j = 0; j < this.dataMap[i].length; j++) {
+          allKeys.push(this.dataMap[i][j][0]);
+        }
+      }
+    }
+    return allKeys;
+  }
 }
 
-const hashTable = new HashTable(2);
-console.log(hashTable.set("naveen", "praksh"));
+const hashTable = new HashTable(9);
+hashTable.set("naveen12123", "prakash");
+hashTable.set("naveen", "pr");
+console.log(hashTable.keys());

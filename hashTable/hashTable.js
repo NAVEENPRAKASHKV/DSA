76883fd169e09b06f7e20e0993d85a4fd09d3b18
naveenprakashkv <naveen.prakash.kv@gmail.com>
2025-01-17 15:44:30 +0530

    set in hashtable added

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -9,4 +9,16 @@ class HashTable {
     }
     return hash;
   }
+
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.dataMap[index]) {
+      this.dataMap[index] = [];
+    }
+    this.dataMap[index].push([key, value]);
+    return this;
+  }
 }
+
+const hashTable = new HashTable(2);
+console.log(hashTable);

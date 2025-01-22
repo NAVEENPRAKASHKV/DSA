f858228c7b9a242124f1e4de5a129eaf92428a62
naveenprakashkv <naveen.prakash.kv@gmail.com>
2025-01-22 18:25:16 +0530

    inset method is added in the class

--- BST/binarySearchTree.js
+++ BST/binarySearchTree.js
@@ -13,9 +13,31 @@ class BST {
   insert(value) {
     const newNode = new Node(value);
     if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
     }
   }
 }
 
 const bst = new BST();
+bst.insert(34);
+bst.insert(35);
+bst.insert(36);
 console.log(bst);
